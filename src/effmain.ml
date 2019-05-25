7d0c07247d1c531ae707ed524d4b7ba6ff74c32c
Ulugbek Abdullaev <ulugbekna@gmail.com>
2019-05-25 13:35:45 +0500

    run "ocamlformat -p janestreet -i src/"

--- src/effmain.ml
+++ src/effmain.ml
@@ -1,11 +1,15 @@
-open Efftester;;
+open Efftester
 
-resetvar();;
-resettypevar();;
+;;
+resetvar ()
+;;
+resettypevar ()
+
+;;
 QCheck_runner.run_tests_main
-  [(* unify_funtest;*)
-   (* gen_classify; *)
-   (* ocaml_test; *)
-   (* tcheck_test; *)
-    eq_test 
+  [ (* unify_funtest;*)
+    (* gen_classify; *)
+    (* ocaml_test; *)
+    (* tcheck_test; *)
+    eq_test
   ]
